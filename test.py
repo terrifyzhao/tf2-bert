705eaa58753a53a4e6ccfb2b8f38880fce2aad30
joe <zjiuzhou@gmail.com>
2020-04-01 17:14:03 +0800

    修改test

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-import tensorflow as tf
-# import tensorflow_datasets
-from transformers import *
+import bert
+import os
+from tensorflow import keras
 
-# Load dataset, tokenizer, model from pretrained model/vocabulary
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-model = TFBertForSequenceClassification.from_pretrained('bert-base-cased')
-# data = tensorflow_datasets.load('glue/mrpc')
-print(123)
+model_dir = "chinese_L-12_H-768_A-12"
+
+bert_params = bert.params_from_pretrained_ckpt(model_dir)
+l_bert = bert.BertModelLayer.from_params(bert_params, name="bert")
+
+max_seq_len = 128
+l_input_ids = keras.layers.Input(shape=(max_seq_len,), dtype='int32')
+l_token_type_ids = keras.layers.Input(shape=(max_seq_len,), dtype='int32')
+
+# using the default token_type/segment id 0
+output = l_bert(l_input_ids)  # output: [batch_size, max_seq_len, hidden_size]
+model = keras.Model(inputs=l_input_ids, outputs=output)
+model.build(input_shape=(None, max_seq_len))
+
+bert_ckpt_file = os.path.join(model_dir, "bert_model.ckpt")
+bert.load_stock_weights(l_bert, bert_ckpt_file)
